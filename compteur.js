b167358c962295dd7ad7669e546e3c0ea0cca398
clara <clara.jacot@gmail.com>
2023-11-23 10:27:51 +0100

    test est notre page, style est le css et compteur le code js

--- compteur.js
+++ compteur.js
@@ -6,11 +6,9 @@ console.log(minute)
 
 let consoPerso = 0
 let compteur = setInterval(calculLitreParSecondeParPersonne,1000); 
-    
- 
 
  function calculLitreParSecondeParPersonne(){
-    console.log((consoPerso.toFixed(2))+" mL");
+    document.getElementById('conso-seconde').innerText ="Depuis que vous êtes connecté.e vous avez consommé "+ (consoPerso.toFixed(2))+" mL";
     consoPerso += 1.73611;
     if (consoPerso>20){
          clearInterval(compteur)  
@@ -23,12 +21,13 @@ function getConsoJour(date) {
     let minuteNow = date.getMinutes();
     let consoHeures =(hourNow*3600)*1.74;
     let consoMinutes = (minuteNow*60)*1.74;
-    let consoJour = ((consoHeures+consoMinutes)/1000).toFixed(2)
-    console.log(consoHeures);
-    console.log(consoMinutes);
-    return "vous avez déjà consommé "+consoJour+" L d'eau potable en moyenne aujourd'hui!"
+    let consoJour = ((consoHeures+consoMinutes)/1000).toFixed(2);
+    //console.log(consoHeures);
+    //console.log(consoMinutes);
+    document.getElementById('conso-jour').innerText += "Vous avez déjà consommé "+consoJour+" L d'eau potable en moyenne aujourd'hui!"
+    //console.log(document.getElementById('conso-jour'))
 }
-console.log(getConsoJour(today))
+getConsoJour(today)
 
 
 // let consoMetropole = 0
