d2e31852a20a1df5c144638a29174413c0bcf026
mariama972 <mbacke.mamediarra@gmail.com>
2023-11-24 16:35:01 +0100

    ajout animation gouttes

--- compteur.js
+++ compteur.js
@@ -39,3 +39,13 @@ getConsoJour(today)
 //          clearInterval(consoMetropole)
 //  }
 // }
+let animation = anime({
+    targets: '.drop',
+    translateY:150,
+    duration: 1500,
+    easing: 'linear',
+    delay: anime.stagger(350),
+    loop: true
+
+    }
+   ) 
