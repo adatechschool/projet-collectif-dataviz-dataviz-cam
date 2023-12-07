a03a67ae64f5d8657f816d336e97ea2c6843c99a
clara <clara.jacot@gmail.com>
2023-12-07 10:04:33 +0100

    meilleur scroll

--- boutons.js
+++ boutons.js
@@ -1,26 +1,14 @@
 const boutonVoirConso = document.querySelector(".voir-conso");
 boutonVoirConso.addEventListener("click", function () {
-  window.scrollTo({
-    top: 699,
-    left: 0,
-    behavior: "smooth",
-  });
+    div2.scrollIntoView({ behavior: "smooth"});
 });
 
 const boutonVoirUtilisation = document.querySelector(".voir-utilisation");
 boutonVoirUtilisation.addEventListener("click", function () {
-  window.scrollTo({
-    top: 1397,
-    left: 0,
-    behavior: "smooth",
-  });
+  div3.scrollIntoView({ behavior: "smooth"});
 });
 
 const boutonVoirNappe = document.querySelector(".voir-nappe");
 boutonVoirNappe.addEventListener("click", function () {
-  window.scrollTo({
-    top: 2096,
-    left: 0,
-    behavior: "smooth",
-  });
+  div4.scrollIntoView({ behavior: "smooth"});
 })
